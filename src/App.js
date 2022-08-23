d564fba41e94d3f052265e31731f744ed0daef9f
Daniel García <danielsv1112@gmail.com>
2022-08-23 14:05:39 -0500

    Avance personal

--- src/App.js
+++ src/App.js
@@ -1,36 +1,54 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 import Tabla from "./componentes/Tabla";
 
 function App() {
   const [datos, setDatos] = new useState(null);
+  const [paginacion, setPaginacion] = new useState(null);
+  const [respuesta, setRespuesta] = new useState(null);
 
-  const consultaApi = () => {
-    fetch('https://api.datos.gob.mx/v1/condiciones-atmosfericas?pageSize=10').then((r) => {return r.json()}).then((respuesta) => {
+  useEffect(() => {
+    fetch('https://api.datos.gob.mx/v1/condiciones-atmosfericas?pageSize=10').then((r) => {
+      return r.json();
+    }).then((respuesta) => {
       setDatos(respuesta.results);
+      setPaginacion(respuesta.pagination);
+      setRespuesta(true);
     });
-  }
-  consultaApi();
-
-  console.log(datos);
+  },[]);
   
-  return (
-    <div className="App">
-      <section className="App-header">
-        <div className='contenedorHeader'>
-          <h1>Condiciones Atmosfericas</h1>
-          <p>By Daniel Garcia 7u7</p>
-        </div>
-
-        <div className='contenedorContenido'>
-          {/* <Tabla datosTabla={} className="miTabla"/> */}
-        </div>
-      </section>
-
-
-    </div>
-  );
+  if(respuesta){
+    return (
+      <div className="App">
+        <section className="App-header">
+          <div className='contenedorHeader'>
+            <h1>Condiciones Atmosfericas</h1>
+            <p>By Daniel Garcia 7u7</p>
+          </div>
+  
+          <div className='contenedorContenido'>
+            <Tabla datosTabla={datos} paginacionTabla={paginacion} className="miTabla"/>
+          </div>
+        </section>  
+      </div>
+    );
+  }else{
+    return (
+      <div className="App">
+        <section className="App-header">
+          <div className='contenedorHeader'>
+            <h1>Condiciones Atmosfericas</h1>
+            <p>By Daniel Garcia 7u7</p>
+          </div>
+  
+          <div className='contenedorContenido'>
+            <div>Cargando datos...</div>
+          </div>
+        </section>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/componentes/Tabla.js
+++ src/componentes/Tabla.js
@@ -1,8 +1,8 @@
 import React from "react";
 
 export default function Tabla(props){
-    var datosTabla = Array.from(props.datosTabla);
-    console.log(datosTabla);
+    var datosTabla = props.datosTabla;
+
     return (
         <div>
             <table className="tabla">
@@ -19,18 +19,20 @@ export default function Tabla(props){
                     </tr>
                 </thead>
                 <tbody>
-                    {/* {datosTabla.map((iterator) => (
-                        // <tr>
-                        //     <td>{iterator._id}</td>
-                        //     <td>{iterator.cityid}</td>
-                        //     <td>{iterator.name}</td>
-                        //     <td>{iterator.state}</td>
-                        //     <td>{iterator.probabilityofprecip}%</td>
-                        //     <td>{iterator.relativehumidity}</td>
-                        //     <td>{iterator.lastreporttime}</td>
-                        //     <td>{(iterator.probabilityofprecip > 60 || iterator.relativehumidity > 50)?'Llueve':'No llueve'}</td>
-                        // </tr>
-                    ))} */}
+                    {datosTabla.map((iterator) => {
+                        return(
+                            <tr id={iterator._id}>
+                                <td>{iterator._id}</td>
+                                <td>{iterator.cityid}</td>
+                                <td>{iterator.name}</td>
+                                <td>{iterator.state}</td>
+                                <td>{iterator.probabilityofprecip}%</td>
+                                <td>{iterator.relativehumidity}</td>
+                                <td>{iterator.lastreporttime}</td>
+                                <td>{(iterator.probabilityofprecip > 60 || iterator.relativehumidity > 50)?'Llueve':'No llueve'}</td>
+                            </tr>
+                        );
+                    })}
                 </tbody>
             </table>
         </div>
